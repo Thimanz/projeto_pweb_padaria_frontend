671d2f48d013740a7b1c026f410ed2ec15caa10a
Thiago Manzi <thiago_manzi@hotmail.com>
2023-04-07 20:47:57 -0300

    projeto_web(build): componente header finalizado e vitrine criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VitrineComponent } from './vitrine/vitrine.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'vitrine',
+    component: VitrineComponent,
+  },
+  // {
+  //   path: '/produto-detalhe',
+  //   component: ProdutoDetalhe,
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
