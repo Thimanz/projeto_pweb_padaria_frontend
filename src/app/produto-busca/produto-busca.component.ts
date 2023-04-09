0bb693b7c3ce65c59ec8024d048e7b35152e6753
Thiago Manzi <thiago_manzi@hotmail.com>
2023-04-08 21:57:06 -0300

    projeto_web(build): produtos.json criado para testes

--- src/app/produto-busca/produto-busca.component.ts
+++ src/app/produto-busca/produto-busca.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-produto-busca',
   templateUrl: './produto-busca.component.html',
-  styleUrls: ['./produto-busca.component.css']
+  styleUrls: ['./produto-busca.component.css'],
 })
-export class ProdutoBuscaComponent {
-
-}
+export class ProdutoBuscaComponent {}
--- src/app/vitrine/vitrine.component.ts
+++ src/app/vitrine/vitrine.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import produtos from '../produtos.json';
+
+interface produto {
+  descricao: String;
+  imagem: String;
+  descricaoCompleta: String;
+}
 
 @Component({
   selector: 'app-vitrine',
   templateUrl: './vitrine.component.html',
-  styleUrls: ['./vitrine.component.css']
+  styleUrls: ['./vitrine.component.css'],
 })
 export class VitrineComponent {
-
+  produtosObject = produtos;
 }
