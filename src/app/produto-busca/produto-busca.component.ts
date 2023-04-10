9372693cc1692d4bd33ce19a95f9df844448ed2e
Bruna Barbosa <bruna.santos158@fatec.sp.gov.br>
2023-04-09 23:50:56 -0300

    projeto_web(build): produto-busca and produtos.json

--- src/app/produto-busca/produto-busca.component.ts
+++ src/app/produto-busca/produto-busca.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import produtos from '../produtos.json';
 
-interface produtos {
-  descricao: String;
-  imagem: String;
-  descricaoCompleta: String;
-  preco: String;
-}
-
 @Component({
   selector: 'app-produto-busca',
   templateUrl: './produto-busca.component.html',
   styleUrls: ['./produto-busca.component.css'],
 })
 export class ProdutoBuscaComponent {
-  produtosObject = produtos;   
+  produtosObject = produtos;
+  returnZero = () => 0;
 }
